9159f35025beee8a918e2b6343ff0253e2015a1a
marcusadigun <adigunmarcus@gmail.com>
2026-04-18 12:51:04 -0400

    Add additional login tests for valid and invalid credentials

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,7 +3,16 @@ import { test, expect } from '@playwright/test';
 test('Login', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     await page.getByRole('textbox', { name: 'Username' }).fill('standard_user');
-    await page.getByRole('textbox', { name: 'Password'}).fill('secret_saththuce');
+    await page.getByRole('textbox', { name: 'Password'}).fill('secret_sauce');
     await page.getByRole("button", { name: 'Login'}).click();
     await expect(page.getByText('Products')).toBeVisible();
   })
+
+test('Login with invalid credentials', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.getByRole('textbox', { name: 'Username' }).fill('standard_user');
+    await page.getByRole('textbox', { name: 'Password'}).fill('secret_saththuce');
+    await page.getByRole("button", { name: 'Login'}).click();
+    await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface')
+})
+
