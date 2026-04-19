5561eed0c8f6c9df08f34ef1962378610ce41d3f
marcusadigun <adigunmarcus@gmail.com>
2026-04-19 15:12:29 -0400

    Add InventoryPage class skeleton for Playwright tests

---
+++ pages/InventoryPage.ts
@@ -0,0 +1,9 @@
+import { Page, expect } from '@playwright/test'
+
+export class InventoryPage {
+    constructor(private page: Page) {}
+
+    async addToCartByName(productName: string) {
+    }
+
+}
