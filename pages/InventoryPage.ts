c20417b48d1a55736b2c38fcfd2dbdaa182ce2d8
marcusadigun <adigunmarcus@gmail.com>
2026-04-19 16:36:05 -0400

    Add utility methods for cart interactions in InventoryPage class

--- pages/InventoryPage.ts
+++ pages/InventoryPage.ts
@@ -4,6 +4,22 @@ export class InventoryPage {
     constructor(private page: Page) {}
 
     async addToCartByName(productName: string) {
+        await this.page.locator('.inventory_item')
+            .filter({ hasText: productName })
+            .getByRole('button', { name: 'Add to cart' })
+            .click()
+    }
+
+    async removeFromCart() {
+        await this.page.getByRole('button', { name: 'Remove' }).click()
+    }
+
+    async getCartBadge() {
+        return this.page.locator('.shopping_cart_badge')
+    }
+
+    async goToCart() {
+        await this.page.locator('.shopping_cart_link').click()
     }
 
 }
