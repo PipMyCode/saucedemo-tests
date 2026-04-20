13b66a300ecb710cf37e0eef8eeeaa3888386d1c
marcusadigun <adigunmarcus@gmail.com>
2026-04-20 12:24:57 -0400

    Add product-specific methods to InventoryPage: openProductByName and removeFromCartByName

--- pages/InventoryPage.ts
+++ pages/InventoryPage.ts
@@ -4,22 +4,35 @@ export class InventoryPage {
     constructor(private page: Page) {}
 
     async addToCartByName(productName: string) {
-        await this.page.locator('.inventory_item')
+        await this.page
+            .locator('.inventory_item')
             .filter({ hasText: productName })
             .getByRole('button', { name: 'Add to cart' })
             .click()
     }
 
-    async removeFromCart() {
-        await this.page.getByRole('button', { name: 'Remove' }).click()
+    async removeFromCartByName(productName: string) {
+        await this.page
+            .locator('.inventory_item')
+            .filter({ hasText: productName})
+            .getByRole('button', { name: 'Remove' })
+            .click()
     }
 
-    async getCartBadge() {
+    getCartBadge() {
         return this.page.locator('.shopping_cart_badge')
     }
 
     async goToCart() {
         await this.page.locator('.shopping_cart_link').click()
+    }
+
+    async openProductByName(productName: string) {
+        await this.page
+            .locator('.inventory_item')
+            .filter({ hasText: productName })
+            .locator('.inventory_item_name')
+            .click()
     }
 
 }
